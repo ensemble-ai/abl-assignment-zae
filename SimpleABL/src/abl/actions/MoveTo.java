9a1a2662523c3e06d5813252f6d37c5fe9ea60e2
Kirsten <kegillam@ucdavis.edu>
2019-02-10 16:19:07 -0800

    get bots to maintain distance from the player

--- SimpleABL/src/abl/actions/MoveTo.java
+++ SimpleABL/src/abl/actions/MoveTo.java
@@ -20,11 +20,13 @@ public class MoveTo extends BaseAction {
 	 *  - 0: target x position
 	 *  - 1: target y position
 	 *  - 2: id of bot
+	 *  - 3: distance to maintain
 	 */
 	public void execute(Object[] args) {
 		int dirx = 0;
 		int diry = 0;
 		int speed = GameEngine.BotSpeed;
+		int dist = (Integer)args[3];
 		
 		for(Bot b:GameEngine.getInstance().getBots()) {
 			if(b.getId() == (Integer)args[2]) {
@@ -40,9 +42,31 @@ public class MoveTo extends BaseAction {
 				}else if(point.getY() - (Integer)args[1] < -speed) {
 					diry = speed;			
 				}
+				
+
+				//check for player
+				if(dist > calcDistance((Integer)args[0], (Integer)args[1], (int)point.getX(), (int)point.getY())) {
+					//move away from player instead of towards
+					dirx = -dirx;
+					diry = -diry;
+				}
+				
 				b.setTrajectory(new Point(dirx, diry));
 				b.setMoved(true);
 			}
 		}
 	}
+	
+	public int calcDistance(int playerX, int playerY, int botX, int botY) {
+		int diffX, diffY;
+		
+		diffX = playerX - botX;
+		diffY = playerY - botY;
+	
+		diffX = diffX*diffX;
+		diffY = diffY*diffY;
+	
+		return (int)Math.sqrt(diffX + diffY); 
+	}
+	
 }
