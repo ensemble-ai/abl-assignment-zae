c580079fcb60edb90d7684427fd1e2ca638f0124
12435554+ASandhuZero@users.noreply.github.com <12435554+ASandhuZero@users.noreply.github.com>
2019-02-14 18:39:24 -0800

    Cleaned merge error in AgentCompiler.

--- SimpleABL/src/abl/build/AgentCompiler.java
+++ SimpleABL/src/abl/build/AgentCompiler.java
@@ -14,9 +14,8 @@ import abl.compiler.Abl;
 public class AgentCompiler {
 	
 	/** enable debug mode? */
-
-	//private static boolean debugMode = true;
-	private static boolean debugMode = false;
+	private static boolean debugMode = true;
+	//private static boolean debugMode = false;
 
 	/**
 	 * Runs the ABL compiler. 
